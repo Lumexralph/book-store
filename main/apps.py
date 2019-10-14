9262e5ab26b7a007ae81f55b686b4e1dae18bf16
Olumide Ogundele <olumideralph@gmail.com>
2019-10-14 19:05:19 +0100

    add signals to generate thumbnail on image upload

--- main/apps.py
+++ main/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    # we need to make sure this file in signals.py is
+    # initialized when the Django application is launched
+    def ready(self):
+        from main import signals
---
+++ main/signals.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+import logging
+
+from PIL import Image
+from django.core.files.base import ContentFile
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from main.models import ProductImage
+
+
+THUMBNAIL_SIZE = (300, 300)
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=ProductImage)
+def generate_thumbnail(sender, instance, **kwargs):
+    logger.info('Generating thumbnail for product %d', instance.id)
+    image = Image.open(instance.image)
+    image = image.convert('RGB')
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+    temp_thumb = BytesIO()          # create a temporary in-memory buffer
+    image.save(temp_thumb, "JPEG")  # save the image as JPEG format
+    temp_thumb.seek(0)              # move the start of the sequence of bytes
+
+    # set save=False otherwise it will run in infinite loop
+    isinstance.thumbnail.save(
+        instance.image.name,
+        ContentFile(temp_thumb.read()),
+        save=False,
+    )
+
+    # close the open file, to free up resources i.e bytes used from memory
+    temp_thumb.close()

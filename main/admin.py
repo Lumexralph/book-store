ce19c0c238cb1a78fdd09c5cf3ff43be3c63207a
Olumide Ogundele <olumideralph@gmail.com>
2019-10-21 06:01:38 +0100

    register the models on the admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from main.models import (
+                        Product,
+                        ProductImage,
+                        ProductTag)
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductTag)
+admin.site.register(ProductImage)

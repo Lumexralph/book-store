5bb3d45f7edbb1fa4f42bc76951d7ce8e5aa00c1
Olumide Ogundele <olumideralph@gmail.com>
2019-10-28 12:50:09 +0100

    user creation  form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.core.mail import send_mail
 import logging
+from django.contrib.auth.forms import (
+    UserCreationForm as DjangoUserCreationForm, UsernameField
+)
+
+from main.models import User
 
 
 logger = logging.getLogger(__name__)
@@ -23,4 +28,25 @@ class ContactForm(forms.Form):
             'site@bookstore.domain',
             ['customerservice@bookstore.domain'],
             fail_silently=False,
+        )
+
+
+class UserCreationForm(DjangoUserCreationForm):
+    class Meta(DjangoUserCreationForm.Meta):
+        model = User
+        fields = ('email',)
+        field_classes = {'email': UsernameField}
+
+    def send_mail(self):
+        logger.info(
+            'Sending account registration email to email=%s',
+            self.cleaned_data['email'],
+        )
+        message = 'Welcome {}'.format(self.cleaned_data['email'])
+        send_mail(
+            'Welcome to BookStore',
+            message,
+            'site@bookstore.domain'
+            [self.cleaned_data['email']],
+            fail_silently=True,
         )

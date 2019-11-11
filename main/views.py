f7da84daa738f636c4666ceff04ae3d78d8bc976
Olumide Ogundele <olumideralph@gmail.com>
2019-11-11 19:14:50 +0100

    create a registration view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
+import logging
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 from main import forms
 from main.models import Product, ProductTag
@@ -38,3 +41,35 @@ class ProductListView(ListView):
             products = Product.objects.active()
 
         return products.order_by('name')
+
+
+logger = logging.getLogger(__name__)
+
+
+class SignupView(FormView):
+    template_name = 'signup.html'
+    form_class = forms.UserCreationForm
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+
+        email = form.cleaned_data.get('email')
+        raw_password = form.cleaned_data.get('password1')
+        logger.info(
+            'New signup for email=%s through SignupView', email
+        )
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+
+        form.send_mail()
+
+        messages.info(
+            self.request, 'Signup was successful'
+        )
+
+        return response
